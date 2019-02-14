be788f6591a24eb1c578a0a9ef685d7493254412
Rúben Silva <rubensilva945@gmail.com>
2019-02-14 18:48:03 +0000

    Features added: randBoolean, randEven, randOdd, randElement

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-random = {
+let random = {
     randInt: function randInt(begin, end) {
-        return Math.ceil(Math.random() * (++end - begin))
+        if (end < begin) { let t = end; end = begin; begin = t}
+        return Math.ceil(Math.random() * (end - begin)) + begin
     },
 
     randFloat: function randFloat(begin, end, dec_places) {
         if (dec_places < 0) throw new RangeError("Decimal places has to be greater or queal than zero")
-        let res = Math.random() * (++end - begin)
+        if (end < begin) { let t = end; end = begin; begin = t}
+        let res = Math.random() * (end - begin) + begin
         return Number(res.toFixed(dec_places))
     },
+
+    randBoolean: function randBoolean() {
+        return !!this.randInt(0, 1);
+    }, 
+
+    randEven: function randEven (begin, end) {
+        if (end < begin) { let t = end; end = begin; begin = t}
+        let res = this.randInt(begin, end);
+        while (res % 2 === 1) res = this.randInt(begin, end);
+        return res
+    },
+
+    randOdd: function randOdd (begin, end) {
+        if (end < begin) { let t = end; end = begin; begin = t}
+        let res = this.randInt(begin, end);
+        while (res % 2 === 0) res = this.randInt(begin, end);
+        return res
+    },
+
+    randElement: function randElement (array) {
+        if(!Array.isArray(array)) throw new Error("Please introduce an array")
+        let index = this.randInt(0, array.length - 1);
+        return array[index]
+    }
 }
 
 module.exports = random
--- sum.test.js
+++ sum.test.js
@@ -8,11 +8,26 @@ test('Generate a random int number between 1 and 5, the result have to be betwee
 
 test('Generate a random float number between 1.33 and 5.27, the result have to be between 1.33 and 5.27', () => {
     const value = random.randFloat(1.33, 5.27, 1);
-    expect(value).toBeGreaterThanOrEqual(1.3)
-    expect(value).toBeLessThanOrEqual(5.3)
+    expect(value).toBeGreaterThan(1.3)
+    expect(value).toBeLessThan(5.3)
 })
 
 test('Generate a random boolean the result has to be true or false', () => {
     const value = random.randBoolean();
     expect(typeof value).toBe("boolean")
+})
+
+test('Generate a random even number the number has to be even', () => {
+    const value = random.randEven(1, 10)
+    expect(value % 2).toBe(0)
+})
+
+test('Generate a random even number the number has to be odd', () => {
+    const value = random.randOdd(1, 10)
+    expect(value % 2).toBe(1)
+})
+
+test('Get a random element from an array the result has to be in array', () => {
+    const value = random.randElement(["JavaScript"])
+    expect(value).toBe("JavaScript")
 })
