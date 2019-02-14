cacd90c83ed07fc39598f290adb968309e98b45d
Rúben Silva <rubensilva945@gmail.com>
2019-02-14 18:08:02 +0000

    Added random float feature

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 random = {
     randInt: function randInt(begin, end) {
         return Math.ceil(Math.random() * (++end - begin))
-    }
+    },
+
+    randFloat: function randFloat(begin, end, dec_places) {
+        if (dec_places < 0) throw new RangeError("Decimal places has to be greater or queal than zero")
+        let res = Math.random() * (++end - begin)
+        return Number(res.toFixed(dec_places))
+    },
 }
 
 module.exports = random
--- sum.test.js
+++ sum.test.js
@@ -7,9 +7,9 @@ test('Generate a random int number between 1 and 5, the result have to be betwee
 })
 
 test('Generate a random float number between 1.33 and 5.27, the result have to be between 1.33 and 5.27', () => {
-    const value = random.randFloat(1.33, 5.27);
-    expect(value).toBeGreaterThanOrEqual(1.33)
-    expect(value).toBeLessThanOrEqual(5.27)
+    const value = random.randFloat(1.33, 5.27, 1);
+    expect(value).toBeGreaterThanOrEqual(1.3)
+    expect(value).toBeLessThanOrEqual(5.3)
 })
 
 test('Generate a random boolean the result has to be true or false', () => {
